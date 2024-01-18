a2856d7eb2593e9a24814dfd050b3265e146ec65
Tomiisme <Hashim.alhassani@outlook.com>
2024-01-18 03:56:26 +0300

    Static Site Generation with getStaticProps - Next.js

--- pages/customers/[id]/orders/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Orders() {
-  return <h1 className="text-4xl">All orders</h1>;
-}
--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -1,3 +1,48 @@
-export default function Customers() {
-  return <h1 className="text-4xl">Customers</h1>;
-}
+import { GetStaticProps, NextPage, InferGetStaticPropsType } from "next";
+
+type Customer = {
+  id: number;
+  name: string;
+  industry: string;
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  return {
+    props: {
+      customers: [
+        {
+          id: 1,
+          name: "John Smith",
+          industry: "Restaurant",
+        },
+        {
+          id: 2,
+          name: "Sal Brown",
+          industry: "Tech",
+        },
+      ] as Customer[],
+    },
+  };
+};
+
+const Customers: NextPage = ({
+  customers,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(customers);
+  return (
+    <>
+      <h1 className="text-4xl">Customers</h1>
+      {customers.map((customer: Customer) => {
+        return (
+          <div key={customer.id}>
+            <p>{customer.name}</p>
+            <p>{customer.industry}</p>
+            <p>{customer.id}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Customers;
